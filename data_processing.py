12993a05f48e6b1e148cf3e3bbda672a51bc7fe4
mallikabera <mallika.bera4@gmail.com>
2024-08-02 22:38:11 +0530

    Added 2 functions

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,10 @@
 print("Adding all the data processing code related to fraud detection")
+a=5
+b=7
+def sum(a,b):
+    return a+b
+def mult(a,b):
+    return a*b
+
+print(sum(a,b))
+print(mult(a,b))
